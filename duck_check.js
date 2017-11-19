bd1bcc3fd1e286ab1a1d2c9e5b6c5a5ce570258b
Theo <theokobb@Theos-MacBook-Air.local>
2017-11-19 12:50:06 -0500

    v1

---
+++ duck_check.js
@@ -0,0 +1,33 @@
+const check_object = (obj, schema) => {
+    for(let key in schema){
+        if(typeof schema[key] === 'object'){
+            check_object(obj[key], schema[key])
+        } else if(typeof obj[key] !== schema[key]){
+            if(typeof obj[key] === 'undefined'){
+                const difference = Object.keys(obj).filter(x => Object.keys(schema).indexOf(x) == -1)
+                throw new Error(
+                    `Expected object to have properties {${Object.keys(schema)}}. Found propert${difference.length > 1? 'ies':'y'} {${difference}}`
+                )
+            } else {
+                throw new Error(
+                    `Expected property of type '${schema[key]}' in object '${JSON.stringify(obj)}.' Got '${obj[key]}' of type '${typeof obj[key]}' instead.`
+                )
+
+            }
+        }
+    }
+
+}
+
+function make_checker(schema){
+    for(let key in schema){
+        if(typeof key !== 'string'){
+            schema[key] = typeof schema[key]
+        }
+    }
+    return obj => {
+        check_object(obj, schema)
+    }
+}
+
+module.exports = make_checker
---
+++ test.js
@@ -0,0 +1,50 @@
+const make_checker = require('./duck_check')
+
+describe('creating a check function', () => {
+    it('should return a function', () => {
+        expect(make_checker({}))
+            .toBeInstanceOf(Function)
+    })
+})
+
+describe('checking objects', () => {
+    const check = make_checker({
+        a: 'number', 
+        b: 'string', 
+        c: {
+            d: 'number'
+        }
+    })
+    const correct = {
+        a: 1, b: 'b', c: { d: 1}
+    }
+    const incorrect_prop_type = {
+        a: 'a', b: 'b', c: { d: 1}
+    }
+    const incorrect_prop = {
+        a2: 'a', b: 'b', c: { d: 1}
+    }
+    const correct_additional_prop = {
+        a: 1, b: 'b', c: { d: 1}, f: 'f'
+    }
+    it('should do nothing when a correct object is given', () => {
+        expect(() => {
+            check(correct)
+        }).not.toThrow()
+    })
+    it('should throw an error when a property has the wrong type', () => {
+        expect(() => {
+            check(incorrect_prop_type)
+        }).toThrow()
+    })
+    it('should throw when a prop that is not in the schema is found', () => {
+        expect(() => {
+            check(incorrect_prop)
+        }).toThrow()
+    })
+    it('should do nothing when the given object has props that are not in the schema', () => {
+        expect(() => {
+            check(correct_additional_prop)
+        }).not.toThrow()
+    })
+})
