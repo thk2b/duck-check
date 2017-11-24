b6fcdea1f588849ee372be8d7bc4df00ae14dade
Theo <theokobb@Theos-MacBook-Air.local>
2017-11-24 18:08:57 -0500

    clean examples

--- example.js
+++ example.js
@@ -1,11 +1,7 @@
 const {assert, check} = require('./index')
 const { not, any, nonEmpty } = require('./index').modifiers
-check([[Number]])([[1,'2','a'],[1,2,'a']]) 
-check(nonEmpty([]))([]) /* false */
-check(not(null))(null)
-console.log(assert(not(any()))(1))
 
 const Person = check({name: String, age: Number})
-// check(either(Number, null))()
-console.log(assert([Person])({name: 'jane', age: 30}, {name: 'john', age: 1}))
-// check(not(null))(null)
+
+check([Person])({name: 'jane', age: 30}, {name: 'john', age: 1})
+
