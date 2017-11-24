4fa05297986afb4fe0cf774af99c17a7a293e960
Theo <theokobb@Theos-MacBook-Air.local>
2017-11-23 19:21:04 -0500

    merge

--- example.js
+++ example.js
@@ -1,15 +1,3 @@
 const check = require('./index')
 
-const Vector = check([Number])
-
-class Player {
-    constructor(position, size){
-        check([Vector, Vector])(arguments)
-        this.position = position
-        this.size = size
-    }
-}
-
-new Player([1,2], ['3',4])
-
-// check(Player)({position: [1,2,'a']})
+check([Function])([console.log, a => {console.log(a)}, ''])
