a83c49a971dbabc1002198fa804eb1f79469f274
Theo <theokobb@pc19.home>
2017-12-11 23:53:24 +0100

    clean example

--- example.js
+++ example.js
@@ -1,6 +1,1 @@
 const {assert, check, modifiers} = require('./index')
-const {add, any, not, nonEmpty} = require('./src/modifiers')
-const { _check } = require('./src/_check')
-
-console.log(assert(any)('a'))
-console.log(assert({a: any})({a: 'a'}))
